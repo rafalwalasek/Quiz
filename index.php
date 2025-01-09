97c1bac0f125382323f45c3a33ee9c2fff22a490
rafal <rafal_walasek@interia.pl>
2025-01-09 12:33:45 +0100

    aktualizacja kodu

--- index.php
+++ index.php
@@ -5,39 +5,56 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tests</title>
 
-    <link rel="stylesheet" href="quiz.css">
+    <link rel="stylesheet" href="tests.css">
+
+    <!--
     <link rel="stylesheet" href="time/time.css">
+-->
 </head>
 <body>
     
-    <header class="header">
+    <!-- header -->
+    <header>
         <div class="logo">
             <img src="images/w.ico" alt="W"><span>T</span>ests.
         </div>
 
-        <nav class="navbar">
+        <nav>
             <a href="#">Home</a>
             <a href="#">About</a>
             <a href="#">Services</a>
             <a href="#" class="active">Tests</a>
             <a href="#">Contact</a>
         </nav>
-    </header>
+    </header> <!-- end header -->
 
-    <main class="main">
+    <!-- main -->
+    <main>
 
-        <div class="container">
-            <section class="quiz-section">
-                <div class="quiz-box">
-                    <h2>Sieci komputerowe</h2>
-                    <div class="quiz-header">
-                        <time id="countdown"></time>
-                    </div>
+        <p>Każdy test składa się z 40 pytań. Aby zdać trzeba uzyskać 50% poprawnych odpowiedzi. Czas na wykonanie testu to 60 min.</p>
 
-                    <?php
-                        // Załączamy plik connect.php, który zawiera połączenie z bazą danych
-                        include('connect.php');
-                    ?>
+        <!-- box section -->
+        <section class="box-section">
+            
+            <div class="box-quiz">
+                <h2>Sieci</h2> 
+                <button class="btn-start">Start</button>
+            </div>
+
+        </section> <!-- end box section -->
+        
+            <section class="home">
+                <div class="home-content">
+                <h2>Sieci komputerowe</h2>
+                <div class="quiz-header">
+                    <time id="countdown"></time>
+                </div>
+
+                    
+                    <!-- <?php
+                        //połączenie z bazą danych
+                        //include('connect.php');
+                    ?> -->
 
                     <div class="quiz-footer">
                         <span class="question-total">1 z 20 Pytań</span>
@@ -50,19 +67,13 @@
                 </div>
             </section>
         
-            <section class="home">
-                <div class="home-content">
-                    <h2>Sieci</h2>
-                    <p>Test składa się z 40 pytań. Aby zdać trzeba uzyskać 50% poprawnych odpowiedzi. Czas na wykonanie testu to 60 min. Powodzenia :)</p>
-                    <button class="start-btn">Start</button>
-                </div>
-            </section>
-        </div>
 
-    </main>
+    </main> <!-- end main -->
 
+    <!--
     <script src="quiz.js"></script>
     <script src="time/time.js"></script>
+-->
 
 </body>
 </html>
